1ee5a79d6b621ef586b0220ea1d3e0c895bb93e5
zhouwang <zouwanyes@163.com>
2022-08-21 15:53:26 +0800

    Some Projects init

---
+++ apps/app/api/internal/handler/apihandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/wangzhou-ccc/mygozero/apps/app/api/internal/logic"
+	"github.com/wangzhou-ccc/mygozero/apps/app/api/internal/svc"
+	"github.com/wangzhou-ccc/mygozero/apps/app/api/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func ApiHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewApiLogic(r.Context(), svcCtx)
+		resp, err := l.Api(&req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}
---
+++ apps/order/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "github.com/wangzhou-ccc/mygozero/apps/order/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
