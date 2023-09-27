dd3557e209e71187e53def3f1074791f7e17b365
wang-zhou <wang.zhou@hp.com>
2023-09-27 15:51:55 +0800

    Some modifications

--- apps/app/api/internal/config/config.go
+++ apps/app/api/internal/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
 	rest.RestConf
@@ -8,4 +11,8 @@ type Config struct {
 		AccessSecret string
 		AccessExpire int64
 	}
+	OrderRPC   zrpc.RpcClientConf
+	ProductRPC zrpc.RpcClientConf
+	ReplyRPC   zrpc.RpcClientConf
+	UserRPC    zrpc.RpcClientConf
 }
--- apps/app/api/internal/svc/servicecontext.go
+++ apps/app/api/internal/svc/servicecontext.go
@@ -2,14 +2,22 @@ package svc
 
 import (
 	"github.com/wangzhou-ccc/mygozero/apps/app/api/internal/config"
+	"github.com/zeromicro/go-zero/zrpc"
+
+	"github.com/wangzhou-ccc/mygozero/apps/order/orderclient"
+	"github.com/wangzhou-ccc/mygozero/apps/product/productclient"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config     config.Config
+	OrderRPC   orderclient.Order
+	ProductRPC productclient.Product
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:     c,
+		OrderRPC:   orderclient.NewOrder(zrpc.MustNewClient(c.OrderRPC)),
+		ProductRPC: productclient.NewProduct(zrpc.MustNewClient(c.ProductRPC)),
 	}
 }
